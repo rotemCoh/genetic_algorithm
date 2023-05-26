4a93f378a7b78a9216501b3346da4e3ca9513990
rotemCoh <rotem11760@gmail.com>
2023-05-26 18:17:58 +0300

    fitness func not working. fixed the mutate, new_generation and crossover funcs.

--- Population.py
+++ Population.py
@@ -1,5 +1,6 @@
 from Person import Person
 import random
+import string
 
 class Population:
     def __init__(self, code, num_of_people):
@@ -22,17 +23,16 @@ class Population:
         population = []
         for p in range(self.num_of_people):
             person = Person(self.code, 0, self.generate_random_alphabet())
+            person.update_code()
             self.people.append(person)
         return population
 
     def get_people(self):
         return self.people
-    def cross_over(self):
-        pass
+
     def fitness(self):
-        word_freq = self.load_word_frequency("dict.txt")
+        word_dict = self.load_word_frequency("dict.txt")
         letter_freq = self.load_letter_frequency("Letter_Freq.txt")
-        print(letter_freq)
         letter2_freq = self.load_letter_frequency("Letter2_Freq.txt")
 
         for person in self.people:
@@ -43,8 +43,8 @@ class Population:
             words = code.split()
             for word in words:
                 clean_word = self.clean_word(word)
-                if clean_word in word_freq:
-                    person_fitness += word_freq[clean_word]
+                if clean_word in word_dict:
+                    person_fitness += word_dict[clean_word]
                 else:
                     # Calculate letter frequency fitness
                     letter_counter = []
@@ -65,12 +65,12 @@ class Population:
                         person.fitness = person_fitness
 
     def load_word_frequency(self, filename):
-        word_freq = {}
+        word_dict = {}
         with open(filename, "r") as file:
             for line in file:
                 word = self.clean_word(line.strip())
-                word_freq[word] = 1  # Assign a fixed frequency of 1 for each word
-        return word_freq
+                word_dict[word] = 1  # Assign a fixed frequency of 1 for each word
+        return word_dict
 
     def load_letter_frequency(self, filename):
         letter_freq = {}
@@ -89,24 +89,119 @@ class Population:
     def new_generation(self):
         new_people = []
         top_five = int(self.num_of_people*0.05)
+        twenty_percent = int(self.num_of_people*0.2)
         temp_fitness = 0
         best_string = ''
         best_dict = {}
         for p in self.people:
-            if p.get_fitnees() > temp_fitness:
-                temp_fitness = p.get_fitnees()
-                best_string = p.get_code()
+            if p.get_fitness() > temp_fitness:
+                temp_fitness = p.get_fitness()
+                best_string = p.get_new_code()
                 best_dict = p.get_alphabet()
 
         for i in range(top_five):
             new_people.append(Person(best_string, 0, best_dict))
-        for a in new_people:
-            print(a.get_code())
 
-    def crossover(self, alphabet1, alphabet2):
+        self.prepering_for_crossover()
+
+        #indexes = list(range(len(self.people)))
+        random_indexes = random.sample(range(len(self.people)), twenty_percent)
+
+        for index in random_indexes:
+            self.people[index].mutate()
+
+        for y in range(self.num_of_people - top_five):
+            new_people.append(self.people[y])
+
+        self.people = new_people
+        self.generations = self.generations + 1
+        #for i in self.people:
+        #    print(i.get_new_code())
+        return best_string , best_dict
+
+    def prepering_for_crossover(self):
+        # Get the indexes of the people
+        indexes = list(range(len(self.people)))
+
+        # Create a list of tuples with two randomly selected people
+        pairs = []
+        while len(indexes) >= 2:
+            random_pair_indexes = random.sample(indexes, 2)
+            random_pair = (self.people[random_pair_indexes[0]], self.people[random_pair_indexes[1]])
+            pairs.append(random_pair)
+            indexes.remove(random_pair_indexes[0])
+            indexes.remove(random_pair_indexes[1])
+
+        for t in pairs:
+            self.crossover(t)
+
+    def crossover(self, couple_of_people):
+        person1, person2 = couple_of_people
+        alphabet1 = person1.get_alphabet()
+        alphabet2 = person2.get_alphabet()
+
         # get the values of every dict
-        values1 = ' '.join(map(str, alphabet1.values()))
-        values2 = ' '.join(map(str, alphabet2.values()))
-        print(values1)
-        print(values2)
+        keys = list(map(str, alphabet1.keys()))
+        values1 = list(map(str, alphabet1.values()))
+        values2 = list(map(str, alphabet2.values()))
+        random_index = random.randint(0, 25)
 
+        # generate the new values
+        crossover_result1 = values1[:random_index] + values2[random_index:]
+        crossover_result2 = values2[:random_index] + values1[random_index:]
+
+        # replace all duplicated letters with none.
+        crossover_result1_with_none = self.add_None_values(crossover_result1)
+        crossover_result2_with_none = self.add_None_values(crossover_result2)
+
+        # finding the values without duplication.
+        crossover_result1_no_dups = self.replace_None_values(crossover_result1_with_none)
+        crossover_result2_no_dups = self.replace_None_values(crossover_result2_with_none)
+
+        new_alphabet1 = {k: v for k, v in zip(keys, crossover_result1_no_dups)}
+        new_alphabet2 = {k: v for k, v in zip(keys, crossover_result2_no_dups)}
+
+        #print("before: " + str(person1.get_alphabet()))
+        person1.alphabet = new_alphabet1
+        #print("after: " + str(person1.get_alphabet()))
+        #print("before: " + str(person1.get_new_code()))
+        person1.update_code()
+        #print("after: " + str(person1.get_new_code()))
+        person2.alphabet = new_alphabet2
+        person2.update_code()
+
+
+
+    # function that replaces duplicated letter with None
+    def add_None_values(self, crossover_result):
+        frequency = {}
+        values_with_none = []
+
+        for letter in crossover_result:
+            frequency[letter] = frequency.get(letter, 0) + 1
+            if frequency[letter] > 1:
+                values_with_none = values_with_none + [None]
+            else:
+                values_with_none = values_with_none + list(letter)
+        return values_with_none
+
+    def replace_None_values(self, crossover_result_with_none):
+        # Create a set from the letters in my_list (excluding None values)
+        existing_letters = [letter for letter in crossover_result_with_none if letter is not None]
+
+        # Create a new list of English letters not in my_list
+        new_list = [letter for letter in string.ascii_lowercase if letter not in existing_letters]
+
+        random.shuffle(new_list)  # Shuffle new_list randomly
+
+        complite_vlues_crossover = []
+        new_list_index = 0
+        for value in crossover_result_with_none:
+            if value is None:
+                random_letter = new_list[new_list_index]
+                complite_vlues_crossover.append(random_letter)
+                new_list_index += 1
+            else:
+                complite_vlues_crossover.append(value)
+
+        return complite_vlues_crossover
--- main.py
+++ main.py
@@ -2,27 +2,57 @@ from Person import Person
 from Population import Population
 import random
 def main():
-    given_code = "xaac, cbz"
-    test = Population(given_code, 10)
+    given_code = "xaac cbz"
+    test = Population(given_code, 200)
     test.generate_random_population()
-    # for i in test.people:
-        # print(i.get_code())
-    # test.fitness()
+    flag_for_generation = 0
+    #for i in test.people:
+     #  print(i.get_new_code())
+    best_string = ""
+    temp_string = ""
+    best_dict = {}
+    temp_dict = {}
+
+
+    while(flag_for_generation < 11):
+        if test.generations == 0:
+            test.fitness()
+            best_string, best_dict = test.new_generation()
+            temp_string = best_string
+        else:
+            test.fitness()
+            temp_string, temp_dict = test.new_generation()
+        if best_string == temp_string:
+            flag_for_generation = flag_for_generation + 1
+            best_dict = temp_dict
+        else:
+            best_string = temp_string
+    print(best_dict)
+    # Open a file in write mode
+    with open("plain.txt", "w") as file:
+        # Write the string to the file
+        file.write(best_string)
+
+    # Open a file in write mode
+    with open("perm.txt", "w") as file:
+        # Iterate over dictionary items and write them to the file
+        for key, value in best_dict.items():
+            file.write(key.lower() + " " + value + "\n")
+
+    #return best_string
+    print(best_string)
+    print(test.generations)
+    #for q in test.people:
+     #  print("new :" + str(i.get_new_code()))
     # for p in test.people:
-    #     # print(p.get_fitnees())
+    #     # print(p.get_fitness())
     #     p.mutate()
 
     # taco = test.get_people()
     # for t in taco:
     #     print(t.get_code())
 
-    selected_people = random.sample(test.get_people(), 2)
-    print(str(selected_people[0]))
-    print(str(selected_people[1]))
-    # alphabet1 = [person["alphabet"] for person in selected_people]
-    # alphabet2 = [person["alphabet"] for person in selected_people]
-    #
-    # test.crossover(alphabet1, alphabet2)
+    #selected_people = random.sample(test.get_people(), 2)
 
 
 if __name__ == "__main__":
